5c4e9e2bb2d9404ea459803ebf1ada4f64c57ad9
Ruan Odendaal <ruanodendaal@gmail.com>
2017-07-04 11:52:35 +0100

    first test on account

---
+++ banking/tests/account_tests.py
@@ -0,0 +1,13 @@
+import unittest
+from account import Account
+
+class TestAccount(unittest.TestCase):
+
+    def setUp(self):
+        self.account = Account(100)
+
+    def test_default_options_set(self):
+        self.assertEquals(self.account.balance, 100)
+
+if __name__ == '__main__':
+    unittest.main()
