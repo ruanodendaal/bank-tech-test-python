d60d2a02418c797ccb558e7c85419e43f61513b3
Ruan Odendaal <ruanodendaal@gmail.com>
2017-07-04 12:44:11 +0100

    failing deposit test

--- banking/tests/account_test.py
+++ banking/tests/account_test.py
@@ -7,10 +7,14 @@ class TestAccount(unittest.TestCase):
         self.account = Account(100)
 
     def test_default_options_set(self):
-        self.assertEquals(self.account.balance, 100)
+        self.assertEqual(self.account.balance, 100)
 
     def test_balance(self):
-        self.assertEquals(self.account.balance(), 100)
+        self.assertEqual(self.account.get_balance(), 100)
+
+    def test_deposit(self):
+        self.account.deposit(50)
+        self.assertEqual(self.account.get_balance(), 150)
 
 if __name__ == '__main__':
     unittest.main()
