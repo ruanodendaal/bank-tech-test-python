f9bfdb28c1b5adc943bc777eb2487cdd71fcc184
Ruan Odendaal <ruanodendaal@gmail.com>
2017-07-06 10:23:23 +0100

    update transaction tests

--- banking/tests/transaction_test.py
+++ banking/tests/transaction_test.py
@@ -4,4 +4,8 @@ from transaction import Transaction
 class TestAccount(unittest.TestCase):
 
     def setUp(self):
-        self.transaction = Transaction(amount, balance)
+        self.transaction = Transaction(100, 150)
+
+    def test_default_options_set(self):
+        self.assertEqual(self.transaction.amount, 100)
+        self.assertEqual(self.transaction.balance, 150)
