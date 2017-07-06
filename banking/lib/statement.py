e2431f5a96c2be6af726813bc77b6c755d98f8af
Ruan Odendaal <ruanodendaal@gmail.com>
2017-07-06 13:53:22 +0100

    add addition print scenarios and make them pass

--- banking/lib/statement.py
+++ banking/lib/statement.py
@@ -7,13 +7,13 @@ class Statement:
         output = "date || credit || debit || balance\n"
         for i in reversed(self.summary):
             if i.amount > 0:
-                output += self.credit(i.amount, i.balance)
+                output += self.credit(i.date, i.amount, i.balance)
             else:
-                output += self.debit(i.amount, i.balance)
+                output += self.debit(i.date, i.amount, i.balance)
         return output
 
-    def credit(self, amount, balance):
-        return "06/07/2017 || %s ||  || %s\n" % (amount, balance)
+    def credit(self, date, amount, balance):
+        return "%s || %s ||  || %s\n" % (date, amount, balance)
 
-    def debit(self, amount, balance):
-        return "||  || %s || %s\n" % (amount, balance)
+    def debit(self, date, amount, balance):
+        return "%s ||  || %s || %s\n" % (date, abs(amount), balance)
--- banking/tests/feature_test.py
+++ banking/tests/feature_test.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from account import Account
 
 class TestFeature(unittest.TestCase):
@@ -6,7 +7,18 @@ class TestFeature(unittest.TestCase):
     def setUp(self):
         self.account = Account(100)
 
-    def test_print(self):
-        output = "date || credit || debit || balance\n06/07/2017 || 50 ||  || 150\n"
+    def test_print_with_one_deposit(self):
+        output = "date || credit || debit || balance\n%s || 50 ||  || 150\n" % time.strftime("%d/%m/%Y")
         self.account.deposit(50)
+        self.assertEqual(self.account.statement.print_statement(), output)
+
+    def test_print_with_one_withdrawl(self):
+        output = "date || credit || debit || balance\n%s ||  || 50 || 50\n" % time.strftime("%d/%m/%Y")
+        self.account.withdraw(50)
+        self.assertEqual(self.account.statement.print_statement(), output)
+
+    def test_print_with_multiple(self):
+        output = "date || credit || debit || balance\n%s ||  || 50 || 70\n%s || 20 ||  || 120\n" % (time.strftime("%d/%m/%Y"), time.strftime("%d/%m/%Y"))
+        self.account.deposit(20)
+        self.account.withdraw(50)
         self.assertEqual(self.account.statement.print_statement(), output)
