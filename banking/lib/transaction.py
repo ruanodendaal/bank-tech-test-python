0529e8c5dd4a9297b76ea88502bdb6ec4d6565b1
Ruan Odendaal <ruanodendaal@gmail.com>
2017-07-06 13:44:06 +0100

    pass transaction date test

--- banking/lib/transaction.py
+++ banking/lib/transaction.py
@@ -1,5 +1,8 @@
+import time
+
 class Transaction:
 
     def __init__(self, amount, balance):
         self.amount = amount
         self.balance = balance
+        self.date = time.strftime("%d/%m/%Y")
