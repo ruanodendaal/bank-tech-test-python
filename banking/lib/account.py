b18c1838e3c4073a76010bc79fd2153b7b8a9158
Ruan Odendaal <ruanodendaal@gmail.com>
2017-07-06 11:11:16 +0100

    correct default options test and make sure transactions get added when deposit or withdrawls are made

--- banking/lib/account.py
+++ banking/lib/account.py
@@ -1,13 +1,24 @@
+from statement import Statement
+from transaction import Transaction
+
 class Account:
 
     def __init__(self, balance = 0):
         self.balance = balance
+        self.statement = Statement()
 
     def get_balance(self):
         return self.balance
 
     def deposit(self, amount):
         self.balance += amount
+        transaction = Transaction(amount, self.balance)
+        update_statement(self, transaction)
 
     def withdraw(self, amount):
         self.balance -= amount
+        transaction = Transaction(-amount, self.balance)
+        update_statement(self, transaction)
+
+def update_statement(self, transaction):
+    self.statement.summary.append(transaction)
--- banking/tests/account_test.py
+++ banking/tests/account_test.py
@@ -8,7 +8,7 @@ class TestAccount(unittest.TestCase):
 
     def test_default_options_set(self):
         self.assertEqual(self.account.balance, 100)
-        self.assertTrue(self.account.statement.summary)
+        self.assertFalse(self.account.statement.summary)
 
     def test_balance(self):
         self.assertEqual(self.account.get_balance(), 100)
@@ -20,3 +20,11 @@ class TestAccount(unittest.TestCase):
     def test_withdrawl(self):
         self.account.withdraw(20)
         self.assertEqual(self.account.get_balance(), 80)
+
+    def test_deposit_transaction_gets_added_in_statement(self):
+        self.account.deposit(50)
+        self.assertTrue(self.account.statement.summary)
+
+    def test_withdrawl_transaction_gets_added_in_statement(self):
+        self.account.withdraw(50)
+        self.assertTrue(self.account.statement.summary)
